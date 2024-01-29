9c04080103a9aca3db53c60a676309b7cae903f8
DD <ddonegal1@gmail.com>
2024-01-29 14:53:34 -0500

    created Deck.js

---
+++ src/Deck.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
+const Deck = () => {
+    const[deckId, setDeckId]= useState(null);
+
+    useEffect(() => {
+        //Function to create new deck and set ID
+      const createNewDeck = async () => {
+        try{
+            const res = await axios.get('https://deckofcardsapi.com/api/deck/new/');
+            setDeckId(res.data.deck_id);
+        } catch(error) {
+            console.error('Error creating a new deck', error);
+        }
+      };
+
+      //Call the function when the component mounts
+      createNewDeck();
+      
+    }, []);
+
+    if(!deckId) {
+        return null; 
+    }
+
+    return(
+        <div>
+            <CardDeck deckId={deckId} />
+        </div>
+    )
+};
